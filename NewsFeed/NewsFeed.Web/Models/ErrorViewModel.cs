269b9b913842fe44abc41625b997417893e39f5b
Shutirtha Roy <shutirtha.roy1@gmail.com>
2024-01-15 17:36:28 +1100

    Project initialized

---
+++ NewsFeed/NewsFeed.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace NewsFeed.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
